6f53b0eeaa174d2896cb6d6f54644818b6f7042b
Stijn Peeters <schwarzie2478@hotmail.com>
2024-12-08 21:06:22 +0100

    some simple prep

---
+++ day11/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+var filename="inputdata.txt";
+var input = File.ReadAllLines(filename);
+
+var result = 0;
+Console.WriteLine($"Result part 1: {result}");
